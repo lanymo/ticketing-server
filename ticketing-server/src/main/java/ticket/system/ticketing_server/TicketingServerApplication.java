e0bb475258975e3c42bdfe4e97e2d02ed8d10a57
lanymo <mien4517@gmail.com>
2026-03-18 18:44:33 +0900

    project creation

---
+++ ticketing-server/src/main/java/ticket/system/ticketing_server/TicketingServerApplication.java
@@ -0,0 +1,13 @@
+package ticket.system.ticketing_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingServerApplication.class, args);
+	}
+
+}
---
+++ ticketing-server/src/test/java/ticket/system/ticketing_server/TicketingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ticket.system.ticketing_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
